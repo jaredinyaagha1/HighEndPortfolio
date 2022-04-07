e9b49edf4a2397cc84db4cb078261adb33b2565f
NIGHT LIGHT <jaredinyaagha@gmail.com>
2022-04-07 00:19:47 -0400

    Vichyssoise

--- highendportfolio/src/App.js
+++ highendportfolio/src/App.js
@@ -1,13 +1,15 @@
 // import logo from './logo.svg';
 // import './App.css';
 
-import React, { useState } from 'react';
+import React from 'react';
 // import Header from './Header';
 
 function App() {
   return (
-    <div className="App">10
+    <div className="App"> Put entire app contents here
+      <p>Vichyssoise</p>
     </div >
+
   );
 }
 
