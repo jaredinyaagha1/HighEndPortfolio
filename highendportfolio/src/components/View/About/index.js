6c825fcecb0d6a959a22068bed507397471b4872
NIGHT LIGHT <jaredinyaagha@gmail.com>
2022-04-24 21:47:37 -0400

    drafting an about page

--- highendportfolio/src/components/View/About/index.js
+++ highendportfolio/src/components/View/About/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import blackGuy1 from '../../../assets/images/BlackGuy1.jpg';
 
 function About() {
     return (
         <div>
             <h1>About</h1>
+            <section className='row'>
+                <div className='col-md-6'>
+                    <img className='profilePic' src={blackGuy1} alt='profile' />
+                </div>
+                <div className='col-md-6'>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </div>
+
+            </section>
         </div>
     );
 }
