17e25aadc35e602dd7c0488c45127351de914c46
NIGHT LIGHT <jaredinyaagha@gmail.com>
2022-05-09 03:26:26 -0400

    it's way passed my bedtime heroku seriously

--- src/index.js
+++ src/index.js
@@ -6,18 +6,18 @@ import './index.scss';
 // Importing the Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const cool = require('cool-ascii-faces');
-const express = require('express');
-const path = require('path');
-const PORT = process.env.PORT || 5000;
+// const cool = require('cool-ascii-faces');
+// const express = require('express');
+// const path = require('path');
+// const PORT = process.env.PORT || 5000;
 
-express()
-    .use(express.static(path.join(__dirname, 'public')))
-    .set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'ejs')
-    .get('/', (req, res) => res.render('pages/index'))
-    .get('/cool', (req, res) => res.send(cool()))
-    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+// express()
+//     .use(express.static(path.join(__dirname, 'public')))
+//     .set('views', path.join(__dirname, 'views'))
+//     .set('view engine', 'ejs')
+//     .get('/', (req, res) => res.render('pages/index'))
+//     .get('/cool', (req, res) => res.send(cool()))
+//     .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 ReactDOM.render(
     <React.StrictMode>
